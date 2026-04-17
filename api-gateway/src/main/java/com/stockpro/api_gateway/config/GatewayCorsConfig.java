0e3105fa475a21ab6503dda664477310e81524f8
Suraj Kumar <suraj1122003@gmail.com>
2026-04-17 23:15:34 +0530

    [Suraj]: Added the api-gateway service

---
+++ api-gateway/src/main/java/com/stockpro/api_gateway/config/GatewayCorsConfig.java
@@ -0,0 +1,26 @@
+package com.stockpro.api_gateway.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GatewayCorsConfig {
+
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost","http://13.206.157.162"));
+		corsConfig.setMaxAge(3600L);
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		corsConfig.setAllowedHeaders(Arrays.asList("*"));
+		corsConfig.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		
+		return new CorsWebFilter(source);
+	}
+}
