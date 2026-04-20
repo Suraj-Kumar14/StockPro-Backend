07e6e5c426ecda6651763a2bd8086ce2909dc703
Suraj Kumar <suraj1122003@gmail.com>
2026-04-21 02:05:16 +0530

    [Suraj] : update LoginRequest DTO validation and login payload structure

--- auth-service/src/main/java/com/stockpro/dtos/LoginRequest.java
+++ auth-service/src/main/java/com/stockpro/dtos/LoginRequest.java
@@ -3,9 +3,11 @@ package com.stockpro.dtos;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
 	
 	@NotNull(message = "email null not allowed")
