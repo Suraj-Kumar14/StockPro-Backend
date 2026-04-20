49fc316973f87b0198463961d0c5b1c092cd7d59
Suraj Kumar <suraj1122003@gmail.com>
2026-04-21 02:05:05 +0530

    [Suraj] : improve UserController auth endpoints and request flow

--- auth-service/src/main/java/com/stockpro/controller/UserController.java
+++ auth-service/src/main/java/com/stockpro/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.stockpro.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -189,6 +190,7 @@ public class UserController {
     }
 
 	@Operation(summary = "Deactivate user account")
+	@PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/deactivate/{id}")
     public ResponseEntity<Void> deactivate(@PathVariable Long id) {
         userService.deactivateUser(id);
